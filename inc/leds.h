563a4d67dc2fc6b5ede2ca4add6a3fa5d1db1f01
eddycar <edicarvajalp@gmail.com>
2024-10-03 10:34:44 -0500

    Se agrega archivo validations.yml para CI

--- inc/leds.h
+++ inc/leds.h
@@ -9,63 +9,63 @@
 #ifndef LEDS_H
 #define LEDS_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 /**
  * @brief Inicializa el puerto y apaga todos los LEDs
- * 
- * @param direccion 
- * @return true 
- * @return false 
+ *
+ * @param direccion
+ * @return true
+ * @return false
  */
 bool LedsCreate(uint16_t *direccion);
 
 /**
  * @brief Enciende un LED
- * 
- * @param led 
- * @return true 
- * @return false 
+ *
+ * @param led
+ * @return true
+ * @return false
  */
 bool LedsSetOn(int led);
 
 /**
  * @brief apaga un LED
- * 
- * @param led 
- * @return true 
- * @return false 
+ *
+ * @param led
+ * @return true
+ * @return false
  */
 bool LedsSetOff(int led);
 
 /**
  * @brief Enciende todos los LEDs
- * 
+ *
  */
 void AllLedsSetOn(void);
 
 /**
  * @brief Apaga todos los LEDs
- * 
+ *
  */
 void AllLedsSetOff(void);
 
 /**
  * @brief Consulta si un LED está prendido
- * 
- * @param led 
- * @return true 
- * @return false 
+ *
+ * @param led
+ * @return true
+ * @return false
  */
 bool LedIsOn(int led);
 
 /**
  * @brief Consulta si un LED está apagado
- * 
- * @param led 
- * @return true 
- * @return false 
+ *
+ * @param led
+ * @return true
+ * @return false
  */
 bool LedIsOff(int led);
 
